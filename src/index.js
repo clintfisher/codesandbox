e61ac4d905c818c69741b4856ae57346df8bfba1
Clint Fisher <clintfisher@users.noreply.github.com>
2018-12-10 22:11:13 -0800

    Update index.js
    housekeeping

--- src/index.js
+++ src/index.js
@@ -5,52 +5,11 @@ import PropTypes from 'prop-types';
 import { Global, css, jsx } from '@emotion/core';
 
 /**
- * @function repeat - repeata a unit value anywhere in code, handle ie syntax
- * @param { number } repetitions
- * @param { string } unit - unit to repeat (fr, px, rem etc)
- * @param { bool } ie - internet explorer 10 & 11
- * @returns { string } - unit repeated
- * @example
- * repeat(4, '3fr', true);
- * // returns (3fr)[4]
- * @example
- * repeat(4, '3fr');
- * // returns 3fr 3fr 3fr 3fr
+ * @object gridSpec - define our grid
+ * mobile
+ * tablet
+ * desktop
  */
-const repeat = (repetitions, unit = '1fr', ie = false) => {
-  let str;
-  ie ? (str = `(${unit})[${repetitions}]`) : (str = unit + ' ');
-  return ie ? str : str.repeat(repetitions);
-};
-
-/**
- * @function gridColumn - places a grid item on a column
- * @param {number} start - grid line column starts at
- * @param {number} end - grid line column ends at
- * @param {bool} span - false to use grid line end rather than span
- */
-const gridColumn = (start, end, span = true) => {
-  return `
-    -ms-grid-column: ${start};
-    -ms-grid-column-span: ${end};
-    grid-column: ${start}/ ${span ? `span ${end}` : end};
-  `;
-};
-
-/**
- * @function gridRow- places a grid item on a row
- * @param {number} start - grid line row starts at
- * @param {number} end - grid line row ends at
- * @param {bool} span - false to use grid line end rather than span
- */
-const gridRow = (start, end, span = true) => {
-  return `
-    -ms-grid-row: ${start};
-    -ms-grid-row-span: ${end};
-    grid-row: ${start}/ ${span ? `span ${end}` : end};
-  `;
-};
-
 const gridSpec = {
   mobile: {
     columns: 6,
@@ -70,6 +29,61 @@ const gridSpec = {
     rows: '3',
     rowGap: '2.5rem'
   }
+};
+
+/**
+ * @function repeat - repeata a unit value anywhere in code, handle ie syntax
+ * @param { number } repetitions
+ * @param { string } expression - expression to repeat (fr, px, rem etc)
+ * @param { bool } ie - internet explorer 10 & 11
+ * @returns { string } - unit repeated
+ * @example
+ * repeat(4, '3fr', true);
+ * // returns (3fr)[4]
+ * @example
+ * repeat(4, '3fr');
+ * // returns 3fr 3fr 3fr 3fr
+ */
+const repeat = (repetitions, expression = '1fr', ie = false) => {
+  let string;
+  ie ? (string = `(${expression})[${repetitions}]`) : (string = expression + ' ');
+  return ie ? string : string.repeat(repetitions);
+};
+
+/**
+ * @function gridColumn - places a grid item on a column
+ * @param {number} start - grid line column starts at
+ * @param {number} end - grid line column ends at
+ * @param {bool} span - false to use grid line end rather than span
+ * @example
+ * ${gridColumn(2, 6)};
+ * // grid-column-start: 2;
+ * // grid-column-end: span 6;
+ * @example
+ * ${gridColumn(1, -1, false)};
+ * // grid-column-start: 1;
+ * // grid-column-end: -1;
+ */
+const gridColumn = (start, end, span = true) => {
+  return `
+    -ms-grid-column: ${start};
+    -ms-grid-column-span: ${end};
+    grid-column: ${start}/ ${span ? `span ${end}` : end};
+  `;
+};
+
+/**
+ * @function gridRow - places a grid item on a row
+ * @param {number} start - grid line row starts at
+ * @param {number} end - grid line row ends at
+ * @param {bool} span - false to use grid line end rather than span
+ */
+const gridRow = (start, end, span = true) => {
+  return `
+    -ms-grid-row: ${start};
+    -ms-grid-row-span: ${end};
+    grid-row: ${start}/ ${span ? `span ${end}` : end};
+  `;
 };
 
 /**
